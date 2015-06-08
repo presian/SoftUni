044ef827a014a8fe8d424a4d1f5ef82858704f78
presian <presiandanailov@gmail.com>
2015-06-08 08:47:56 +0300

    Add project

---
+++ ExamPreparations/ASP.NET_MVC/Bookmarks/Bookmarks.Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace Bookmarks.Models
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class User : IdentityUser
+    {
+        public User()
+        {
+            this.Votes = new List<Vote>();
+            this.Comments = new List<Comment>();
+            this.Bookmarks = new List<Bookmark>();
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public virtual ICollection<Vote> Votes { get; set; }
+
+        public virtual ICollection<Bookmark> Bookmarks { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
