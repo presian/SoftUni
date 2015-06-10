848b193a99744f0d9566f4a0594fecaf696eddf6
presian <presiandanailov@gmail.com>
2015-06-10 18:17:49 +0300

    Add some new display templates

---
+++ ExamPreparations/ASP.NET_MVC/Bookmarks/Bookmarks.Web/Areas/User/Models/DisplayModels/CommentDisplayModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookmarks.Web.Areas.User.Models.DisplayModels
+{
+    public class CommentDisplayModel
+    {
+        public int Id { get; set; }
+        
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+    }
+}
