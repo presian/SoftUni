43cf6bcddffd53fc81557e899470f46c4490983f
presian <presiandanailov@gmail.com>
2015-07-08 08:39:02 +0300

    Add first two problems

---
+++ Datastructures/HomeWorks/_2_LinearDataStructures/_1_SumAndAverage/SumAndAverageMain.cs
@@ -0,0 +1,32 @@
+﻿namespace _1_SumAndAverage
+{
+    using System;
+    using System.Collections.Generic;
+
+    static class SumAndAverageMain
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter numbers separate with space!");
+            var userInput = Console.ReadLine();
+            if (string.IsNullOrEmpty(userInput))
+            {
+                userInput = "0 0";
+            }
+            
+            var numbersAsString = userInput.Split(' ');
+            var sumOfNumbers = 0;
+            var integersCount = 0;
+            var integers = new List<int>();
+            foreach (var number in numbersAsString)
+            {
+                var currentNumber = int.Parse(number);
+                sumOfNumbers += currentNumber;
+                integersCount += 1;
+                integers.Add(currentNumber);
+            }
+
+            Console.WriteLine(string.Format("Sum = {0}; Average = {1}", sumOfNumbers, (double)sumOfNumbers/integersCount));
+        }
+    }
+}
---
+++ Datastructures/HomeWorks/_2_LinearDataStructures/_2_SortWords/SortWords.cs
@@ -0,0 +1,20 @@
+﻿namespace _2_SortWords
+{
+    using System;
+    using System.Linq;
+
+    class SortWords
+    {
+        static void Main()
+        {
+            var inputRow = Console.ReadLine();
+            if (string.IsNullOrEmpty(inputRow))
+            {
+                throw new NullReferenceException("Invalid input.");
+            }
+
+            var sortedArr = inputRow.Split().OrderBy(s => s);
+            Console.WriteLine(String.Join(" ", sortedArr));
+        }
+    }
+}
