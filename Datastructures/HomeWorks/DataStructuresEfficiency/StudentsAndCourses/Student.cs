cdf8e4e83bd3ed1e170fcd8a8485c16be1e6e322
presian <presiandanailov@gmail.com>
2015-09-01 22:21:32 +0300

    add first problem form last homework

---
+++ Datastructures/HomeWorks/DataStructuresEfficiency/StudentsAndCourses/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace StudentsAndCourses
+{
+    using System;
+
+    class Student : IComparable<Student>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            var lastNamesComare = string.CompareOrdinal(this.LastName, other.LastName);
+            if (lastNamesComare == 0)
+            {
+                return string.CompareOrdinal(this.FirstName, other.FirstName);
+            }
+            else
+            {
+                return lastNamesComare;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
---
+++ Datastructures/HomeWorks/DataStructuresEfficiency/StudentsAndCourses/StudentsAndCoursesMain.cs
@@ -0,0 +1,53 @@
+﻿namespace StudentsAndCourses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class StudentsAndCoursesMain
+    {
+        static void Main()
+        {
+            var dict = new SortedDictionary<string, SortedSet<Student>>();
+            StreamReader streamReader = new StreamReader(@"../../students.txt");
+            string line = streamReader.ReadLine();
+            while (line != null)
+            {
+                var inputParts = line.Split(new[] {'|'}, StringSplitOptions.RemoveEmptyEntries);
+                var dictKey = inputParts[2].Trim();
+                if (!dict.ContainsKey(dictKey))
+                {
+                    dict.Add(dictKey, new SortedSet<Student>
+                    {
+                        new Student
+                        {
+                            FirstName = inputParts[0].Trim(),
+                            LastName = inputParts[1].Trim()
+                        }
+                    });
+                }
+                else
+                {
+                    dict[dictKey].Add(new Student
+                    {
+                        FirstName = inputParts[0].Trim(),
+                        LastName = inputParts[1].Trim()
+                    });
+                }
+
+                line = streamReader.ReadLine();
+            }
+
+            PirntResult(dict);
+        }
+
+        private static void PirntResult(SortedDictionary<string, SortedSet<Student>> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}: {string.Join(", ", item.Value.Select(s=>s.ToString()))}");
+            }
+        }
+    }
+}
