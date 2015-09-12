1906a2e5a44892bb626c52915cc9b6244333c555
presian <presiandanailov@gmail.com>
2015-09-12 21:31:52 +0300

    add version 2 first problem

--- Datastructures/Exercises/10. Exam-Prepartion.v2/Collection-of-Persons/Collection-of-Persons/PersonCollectionSlow.cs
+++ Datastructures/Exercises/10. Exam-Prepartion.v2/Collection-of-Persons/Collection-of-Persons/PersonCollectionSlow.cs
@@ -1,59 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public class PersonCollectionSlow : IPersonCollection
 {
-    // TODO: define the underlying data structures here ...
+    private int count;
+
+    public PersonCollectionSlow()
+    {
+        Persons = new List<Person>();
+    }
+
+    public List<Person> Persons { get; set; } 
 
     public bool AddPerson(string email, string name, int age, string town)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        var person = new Person
+        {
+            Age = age,
+            Email = email,
+            Name = name,
+            Town = town
+        };
+        if (this.Persons.Contains(person))
+        {
+            return false;
+        }
+
+        this.Persons.Add(person);
+        this.count++;
+        return true;
     }
 
-    public int Count
-    {
-        get
-        {
-            // TODO: implement this
-            throw new NotImplementedException();
-        }
-    }
+    public int Count => this.count;
 
     public Person FindPerson(string email)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        return this.Persons.FirstOrDefault(p => p.Email == email);
     }
 
     public bool DeletePerson(string email)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        var person = this.Persons.FirstOrDefault(p => p.Email == email);
+        if (person == null)
+        {
+            return false;
+        }
+
+        this.Persons.Remove(person);
+        this.count--;
+        return true;
     }
 
     public IEnumerable<Person> FindPersons(string emailDomain)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        return this.Persons
+            .Where(p => p.Email.Contains(emailDomain))
+            .OrderBy(p => p.Email);
     }
 
     public IEnumerable<Person> FindPersons(string name, string town)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        return this.Persons
+            .Where(p => p.Name.Equals(name) 
+                && p.Town.Equals(town))
+            .OrderBy(p => p.Email);
     }
 
     public IEnumerable<Person> FindPersons(int startAge, int endAge)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        return this.Persons
+            .Where(p => p.Age >= startAge 
+                && p.Age <= endAge)
+            .OrderBy(p => p.Age)
+            .ThenBy(p => p.Email);
     }
 
     public IEnumerable<Person> FindPersons(
         int startAge, int endAge, string town)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+        return this.Persons
+            .Where(p => p.Town.Equals(town)
+                        && p.Age >= startAge
+                        && p.Age <= endAge);
     }
 }
