9243c20087b8b7f604dc347526af78621d985c42
presian <presiandanailov@gmail.com>
2015-09-29 22:37:15 +0300

    add unfinished second homework

---
+++ Algorithms/HomeWorks/CombinatorialAlgorithms/LoopPermutations/LoopPermutationsMain.cs
@@ -0,0 +1,34 @@
+﻿namespace LoopPermutations
+{
+    using System;
+    using System.Linq;
+
+    class LoopPermutationsMain  
+    {
+        static int totalPermutationsCount = 0;
+
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var array = Enumerable.Range(1, n).ToArray();
+            var controlArray = Enumerable.Range(1, n).ToArray();
+
+
+            MakePermutation(array, controlArray, 0);
+            Console.WriteLine($"Total permutations: {totalPermutationsCount}");
+        }
+
+        private static void MakePermutation(int[] array, int[] contolArray, int index)
+        {
+            while (true)
+            {
+                
+            }
+        }
+
+        private static void PirntResult(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ Algorithms/HomeWorks/CombinatorialAlgorithms/Permutations/PermutationsMain.cs
@@ -0,0 +1,48 @@
+﻿namespace Permutations
+{
+    using System;
+    using System.Linq;
+
+    class PermutationsMain
+    {
+        static int totalPermutationsCount = 0;
+
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var array = Enumerable.Range(1, n).ToArray();
+            var used = new bool[n];
+
+
+            MakePermutation(array, used, 0);
+            Console.WriteLine($"Total permutations: {totalPermutationsCount}");
+        }
+
+        private static void MakePermutation(int[] array, bool[] used, int index)
+        {
+            if (index == array.Length)
+            {
+                totalPermutationsCount++;
+                PirntResult(array);
+            }
+            else
+            {
+                for (int i = 1; i <= array.Length; i++)
+                {
+                    if (used[i-1] == false)
+                    {
+                        array[index] = i;
+                        used[i - 1] = true;
+                        MakePermutation(array, used, index + 1);
+                        used[i - 1] = false;
+                    }
+                }
+            }
+        }
+
+        private static void PirntResult(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
