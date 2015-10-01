6cff3848508eb72d16ef7614e76d21d971d50ff0
presian <presiandanailov@gmail.com>
2015-10-01 09:39:14 +0300

    add new problem to homework

---
+++ Algorithms/HomeWorks/CombinatorialAlgorithms/LoopCombinations/LoopCombinationsMain.cs
@@ -0,0 +1,46 @@
+﻿namespace LoopCombinations
+{
+    using System;
+    using System.Linq;
+
+    static class LoopCombinationsMain
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            var array = Enumerable.Range(1, k).ToArray();
+            bool done = false;
+            while (!done)
+            {
+                // Show the current one and set up for the next one
+                // The next two lines just output the array.
+                // Change them to whatever you need them to be to output the array
+                Console.WriteLine(string.Join(" ", array));
+
+                // Find the last element in the array that can be incremented
+                int index = k - 1;
+                int position_max = n;
+                while (index >= 0 && array[index] == position_max)
+                {
+                    position_max--;
+                    index--;
+                }
+                if (index < 0)
+                {
+                    done = true;
+                }
+                else
+                {
+                    // increment
+                    array[index]++;
+                    // reset the ones to the right
+                    for (int i = index + 1; i < k; ++i)
+                    {
+                        array[i] = array[i - 1] + 1;
+                    }
+                }
+            }
+        }
+    }
+}
--- Algorithms/HomeWorks/CombinatorialAlgorithms/SubsetOfStringArray/SubsetOfStringArrayMain.cs
+++ Algorithms/HomeWorks/CombinatorialAlgorithms/SubsetOfStringArray/SubsetOfStringArrayMain.cs
@@ -6,7 +6,33 @@
     {
         static void Main()
         {
-            Console.WriteLine('a');
+            var s = new string[]
+            {
+                "test",
+                "rock",
+                "fun"
+            };
+
+            var k = 2;
+            var subset = new string[k];
+            MakeSubsets(s, k, subset, 0);
+
+        }
+
+        private static void MakeSubsets(string[] s, int k, string[] subset, int index)
+        {
+            if (index >= k)
+            {
+                Console.WriteLine($"({string.Join(" ", subset)})");
+            }
+            else
+            {
+                for (int i = index; i < s.Length; i++)
+                {
+                    subset[index] = s[i];
+                    MakeSubsets(s, k, subset, i + 1);
+                }
+            }
         }
     }
 }
