8a8b6108c809ec70e8b48a08f641f80bb9225b60
presian <presiandanailov@gmail.com>
2015-10-06 07:04:09 +0300

    second homework is done

---
+++ Algorithms/HomeWorks/CombinatorialAlgorithms/PermutationWithRepetition/PermutationWithRepetitionMain.cs
@@ -0,0 +1,52 @@
+﻿namespace PermutationWithRepetition
+{
+    using System;
+
+    class PermutationWithRepetitionMain
+    {
+        static void Main()
+        {
+//            var set = new int[] {1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+            var set = new int[] { 1, 3, 5, 5 };
+            Permute(set, 0, set.Length);
+        }
+
+
+        public static void Permute(int[] numbers, int start, int resultSetCount)
+        {
+            PrintResult(numbers);
+            int tmp = 0;
+
+            if (start < resultSetCount)
+            {
+                for (int i = resultSetCount - 2; i >= start; i--)
+                {
+                    for (int j = i + 1; j < resultSetCount; j++)
+                    {
+                        if (numbers[i] != numbers[j])
+                        {
+                            // swap ps[i] <--> ps[j]
+                            tmp = numbers[i];
+                            numbers[i] = numbers[j];
+                            numbers[j] = tmp;
+
+                            Permute(numbers, i + 1, resultSetCount);
+                        }
+                    }
+
+                    // Undo all modifications done by
+                    // recursive calls and swapping
+                    tmp = numbers[i];
+                    for (int k = i; k < resultSetCount - 1;)
+                        numbers[k] = numbers[++k];
+                    numbers[resultSetCount - 1] = tmp;
+                }
+            }
+        }
+
+        private static void PrintResult(int[] resultSet)
+        {
+            Console.WriteLine($"{{{string.Join(",", resultSet)}}}");
+        }
+    }
+}
--- Algorithms/HomeWorks/CombinatorialAlgorithms/SubsetOfStringArray/SubsetOfStringArrayMain.cs
+++ Algorithms/HomeWorks/CombinatorialAlgorithms/SubsetOfStringArray/SubsetOfStringArrayMain.cs
@@ -4,6 +4,7 @@
 
     static class SubsetOfStringArrayMain
     {
+        private static int greatest = 0;
         static void Main()
         {
             var s = new string[]
@@ -16,7 +17,6 @@
             var k = 2;
             var subset = new string[k];
             MakeSubsets(s, k, subset, 0);
-
         }
 
         private static void MakeSubsets(string[] s, int k, string[] subset, int index)
@@ -25,12 +25,21 @@
             {
                 Console.WriteLine($"({string.Join(" ", subset)})");
             }
-            else
+            else 
             {
                 for (int i = index; i < s.Length; i++)
                 {
-                    subset[index] = s[i];
-                    MakeSubsets(s, k, subset, i + 1);
+                    if (i == 0)
+                    {
+                        subset[index] = s[i];
+                        MakeSubsets(s, k, subset, i + 1);
+                    }
+                    else if (i > greatest)
+                    {
+                        subset[index] = s[i];
+                        greatest = i;
+                        MakeSubsets(s, k, subset, i + 1);
+                    }
                 }
             }
         }
